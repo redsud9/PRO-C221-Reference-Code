58593bdf00c38c2ba9a879524661a11c45d22b3b
redsud9 <140234634+redsud9@users.noreply.github.com>
2024-03-17 13:35:43 +0530

    script.js

--- public/script.js
+++ public/script.js
@@ -107,6 +107,25 @@ $(function () {
         }
     })
 
+    $("#invite_button").click(function() {
+        const to = prompt("Enter the email address")
+        let data = {
+            url: window.location.href,
+            to: to
+        }
+        $.ajax({
+            url:"/send-mail",
+            type:"post",
+            data: JSON.stringify(data),
+            dataType:'application/json',
+            success: function(result){
+                alert("Invite sent!")
+            },
+            error: function(result){
+                console.log(result.responseJSON)
+            }
+        })
+    })
 })
 
 peer.on("open", (id) => {
